9cd4ec91d44c50db89eabd88445eb4a7888d5e87
Sini <siniesofia@protonmail.com>
2021-11-23 14:19:36 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,103 @@
+const express = require('express')
+
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(express.json())
+
+morgan.token('person', (request, response) => {
+    return JSON.stringify(request.body)
+  })
+  
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
+
+
+let persons = [
+  {
+    id: 1,
+    name: "Amira", 
+    number: "098765",
+  },
+  {
+    id: 2,
+    name: "Elina", 
+    number: "098765",
+  },
+  {
+    id: 3,
+    name: "Ensio", 
+    number: "098765",
+  },
+]
+
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world!</h1>')
+})
+
+app.get('/info', (req, res) => {
+    const length = persons.length
+    res.send(`Phonebook has info for ${length} people <br> ${new Date()}`)
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+
+  })
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+const generateId = () => {
+    // const maxId = persons.length > 0
+    //     ? Math.max(...persons.map(p => p.id))
+    //     : 0
+    // return maxId +1
+    return Math.floor(Math.random() * 100)
+}
+
+app.post('/api/persons/', (request, response) => {
+    const body = request.body
+
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+
+    if (!body.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number,
+    }
+
+    persons = persons.concat(person)
+    response.json(person)
+})
+  
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
