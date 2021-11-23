7426e4470b0a4ef00eb7f3d1d42b86a4899a31ae
Sini <siniesofia@protonmail.com>
2021-11-23 14:54:10 +0200

    .gitignore ja Procfile lisätty

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
-
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('person', (request, response) => {
     return JSON.stringify(request.body)
@@ -97,7 +98,7 @@ app.post('/api/persons/', (request, response) => {
 })
   
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
