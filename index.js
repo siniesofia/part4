8ed45b51cbe3d82f8de147d50ed04b2c39187d35
Sini <siniesofia@protonmail.com>
2021-11-24 12:31:30 +0200

    uibuild

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('person', (request, response) => {
     return JSON.stringify(request.body)
