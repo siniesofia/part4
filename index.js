131429661a85d9626aacee247ad157222c6f461c
Sini <siniesofia@protonmail.com>
2021-11-25 17:15:12 +0200

    uibuild

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
-    return response.status(404).send({ error: error.message })
+    return response.status(404).send(error)
   }
 
   next(error)
